15599a2a1c7f16da368933e8c6b03fea07d8dd94
i-bakhsh <igbmadinah@gmail.com>
2018-03-28 14:08:48 +0300

    fix jquery with webpack

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const arr = ["JavaScript","Node","webpack"];
 console.log(arr);
 
+
+//test jquery 
+$(".formClass").append("Ibrahim Bakhsh");
--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,25 @@ module.exports = {
     output: {
         filename: 'bundle.js'
     },
+    resolve: {
+        alias: {
+            vuejs: 'node_modules/vue/dist/vue.min.js',
+            jquery: 'node_modules/jquery/dist/jquery.min.js'
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: require.resolve("jqeury"),
+                use: "imports-loader?this=>window"
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
+    ],
     mode: 'development'
 };
