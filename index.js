1fc35579d1c5039013a10f971a2e785dc4f433ba
Ibrahim Bakhsh <igbmadinah@gmail.com>
2018-04-02 02:52:34 +0300

    fix loading underscore and jquery

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+
 const arr = ["JavaScript","Node","webpack"];
 console.log(arr);
 
 
+
+import $ from 'jquery';
+
+
 //test jquery 
 $(".formClass").append("Ibrahim Bakhsh");
+
+
+import _ from 'underscore';
+ 
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,54 @@
+var webpack = require('webpack');
+const {resolve} = require('path');
+const path = require('path');
+var node_dir = __dirname + '/node_modules';
+//var nodeExternals = require('webpack-node-externals');
+
 module.exports = {
     entry: './index.js',
     output: {
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        path: resolve(__dirname,'dist')
     },
     resolve: {
         alias: {
-            vuejs: 'node_modules/vue/dist/vue.min.js',
-            jquery: 'node_modules/jquery/dist/jquery.min.js'
+            jquery: resolve(node_dir, 'jquery/dist/jquery.min.js'),
+            underscore: 'lodash'
         }
     },
+    //target: 'node',
+    //externals: [nodeExternals()],
     module: {
         rules: [
             {
-                test: require.resolve("jqeury"),
-                use: "imports-loader?this=>window"
+                test: /\.(js|jsx)$/,
+                use: {
+                    loader: 'babel-loader',
+                    query: { presets: ['es2015'] } 
+                },
+                exclude: resolve(__dirname,'node_modules'),
+                include: [
+                    resolve(node_dir, 'jquery/dist/jquery.min.js'),
+                    resolve(node_dir, 'underscore/underscore.min.js'),
+                ]
+                
+            },
+            {
+                test: /\.css$/,
+                loader: 'style!css'
             }
         ]
     },
     plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        /*new webpack.ProvidePlugin({
+            _: 'underscore'
+        }),*/
         new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery"
+            _: 'lodash'
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery'
         })
     ],
     mode: 'development'
